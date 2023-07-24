c1e5a9b71a644546b1e154cd459152db6d4a5145
Sushil Kumar <2020pietadsushil57@poornima.org>
2023-07-24 10:37:13 +0530

    second commit

---
+++ Data Types.py
@@ -0,0 +1,21 @@
+# Data Types 
+
+int 10
+float 10.0
+complex 10+0j
+str 'hi' "hi" '''hi'''  """hi"""
+list [15,9,'hi',9.3] unstructured, mutable collection of het data 
+tuple (5,9,"hi",9.3) 5,9,"hi",9.3 structured, immutable collection of het data
+dict {4:5, 'a': [9,3,4]} structured, mutable collection of key-value pairs
+set {5,7,9,'hi'} unordered collection of unique items
+bool True False
+NoneType None
+
+
+
+# concept of Mutability
+Works on Collection only
+i. Item Access
+   -Indexing
+   -Sub-scripting
+   -Slicing
