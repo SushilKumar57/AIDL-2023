1670d0675e630a4662da4946dc3e11a2f4707c68
Sushil Kumar <2020pietadsushil57@poornima.org>
2023-08-16 10:14:28 +0530

    fifth commit

---
+++ # import day5_lib.py
@@ -0,0 +1,9 @@
+# import day5_lib
+from day5_lib import multiplier
+import day5_lib as d5
+
+values = input("Enter values seperated by comma(,): ")
+values = [eval(val) for ]val in values.split(',')]
+print(values)
+print(mt(*values))
+print(data * 3)
